3ecdb5447189e0d32f24c51becdfc320a676763b
Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>
2022-10-12 20:31:25 +0300

    Take advantage of `start` to spawn new CMD windows to run package mgrs (#2)
    * Since installing package managers require shell restart because
      they add themselves to PATH, we need to restart the shell or spawn
      new CMD window for each package installation process (this also
      gives us availability to perform install tasks in parallel :D).
      Let's take advantage of `start` in combo with `cmd` with `/C`
      switch to do this for us.
    
    * Commit 2 of 2, as an alternative to make the shell restart.
    
    Signed-off-by: Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>

--- run_mkdocs_required_app_install.bat
+++ run_mkdocs_required_app_install.bat
@@ -3,9 +3,10 @@
 
 Powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install pandoc
-choco install rsvg-convert
-choco install miktex
+rem Take advantage of "start" spawning a new instance of a process.
+start cmd /C choco install pandoc
+start cmd /C choco install rsvg-convert
+start cmd /C choco install miktex
 
 rem We need newer version of App Installer to get winget functionality.
 rem We presume that the person has installed the latest updates.
@@ -15,7 +16,7 @@ rem Aligned with latest revision of Scoop documentation and PowerShell
 rem syntaxing.
 Powershell.exe iex \"& {$(irm get.scoop.sh)} -RunAsAdmin\"
 Powershell.exe Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-scoop install marp
+start cmd /C scoop install marp
 
 pip install mkdocs
 pip install pymdown-extensions
